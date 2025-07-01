07b65e623f450bbc775f69197a85d1d7bdf77122
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-01 10:51:15 +0900

    feat : weather, modifiedAt 조건 검색 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+
+    Page<Todo> findAllByWeatherOrderByModifiedAt(String weather, Pageable pageable);
+
+    Page<Todo> findAllByModifiedAtBetweenOrderByModifiedAt(LocalDateTime modifiedAtAfter, LocalDateTime modifiedAtBefore, Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -47,10 +49,18 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDateTime modifiedAtStart, LocalDateTime modifiedAtEnd) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        Page<Todo> todos;
+
+        if(!(weather == null)) {
+            todos = todoRepository.findAllByWeatherOrderByModifiedAt(weather, pageable);
+        } else if (modifiedAtStart != null && modifiedAtEnd != null) {
+            todos = todoRepository.findAllByModifiedAtBetweenOrderByModifiedAt(modifiedAtStart, modifiedAtEnd, pageable);
+        } else {
+            todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        }
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
